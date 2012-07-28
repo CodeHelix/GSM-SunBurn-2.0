6e7a71bdbc6de59bbe19e52e59795187f3ab2d65
David Cox <dwc2987@yahoo.com>
2012-07-27 22:54:22 -0500

    first commit

---
+++ GameStateManagementSample/GameStateManagementSample/Game.cs
@@ -0,0 +1,246 @@
+//-----------------------------------------------
+// Synapse Gaming - SunBurn Starter Kit
+//-----------------------------------------------
+//
+// Provides an empty solution for creating new SunBurn based games and
+// projects.
+// 
+// To use:
+//   -Run the solution from Visual Studio
+//   -When running press F11 to open the in-game editor
+//   -Import new models into the content repository (using the Scene Object tab)
+//   -Drag models from the repository into the scene tree-view
+//   -Add lights, adjust materials, the environment, and more
+//
+// Please see the included Readme.htm for details and documentation.
+//
+//-----------------------------------------------------------------------------
+
+#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+
+// Include the necessary SunBurn namespaces.
+using SynapseGaming.LightingSystem.Core;
+using SynapseGaming.LightingSystem.Collision;
+using SynapseGaming.LightingSystem.Editor;
+using SynapseGaming.LightingSystem.Rendering;
+
+using GameStateManagement;
+#endregion
+
+
+namespace GameStateManagementSample
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class StarterGame : Microsoft.Xna.Framework.Game
+    {
+        #region Fields
+
+        // Default XNA members.
+        public GraphicsDeviceManager graphics;
+
+        // Screen Manager
+        ScreenManager screenManager;
+        ScreenFactory screenFactory;
+
+        #endregion
+
+        public static StarterGame Instance;
+
+        #region Initialization Methods
+
+        public StarterGame()
+        {
+            // Default XNA setup.
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            // Required for lighting system.
+            graphics.PreferredDepthStencilFormat = DepthFormat.Depth24Stencil8;
+
+            // Frame rate is 30 fps by default for Windows Phone.
+            TargetElapsedTime = TimeSpan.FromTicks(333333);
+
+            // Create the screen factory and add it to the Services
+            screenFactory = new ScreenFactory();
+            Services.AddService(typeof(IScreenFactory), screenFactory);
+
+            //Create a new instance of the Screen Manager
+            screenManager = new ScreenManager(this);
+            Components.Add(screenManager);
+
+            Instance = this;
+
+#if WINDOWS_PHONE
+            graphics.IsFullScreen = true;
+
+            // Choose whether you want a landscape or portait game by using one of the two helper functions.
+            InitializeLandscapeGraphics();
+            // InitializePortraitGraphics();
+#else
+            graphics.IsFullScreen = false;
+#endif
+
+#if WINDOWS_PHONE
+            // Hook events on the PhoneApplicationService so we're notified of the application's life cycle
+            Microsoft.Phone.Shell.PhoneApplicationService.Current.Launching +=
+                new EventHandler<Microsoft.Phone.Shell.LaunchingEventArgs>(GameLaunching);
+            Microsoft.Phone.Shell.PhoneApplicationService.Current.Activated +=
+                new EventHandler<Microsoft.Phone.Shell.ActivatedEventArgs>(GameActivated);
+            Microsoft.Phone.Shell.PhoneApplicationService.Current.Deactivated +=
+                new EventHandler<Microsoft.Phone.Shell.DeactivatedEventArgs>(GameDeactivated);
+#else
+            // On Windows and Xbox we just add the initial screens
+            AddInitialScreens();
+#endif
+
+            // AudioManager.Initialize(this);
+
+
+            // Required for lighting system.
+            screenManager.splashScreenGameComponent = new SplashScreenGameComponent(this);
+            Components.Add(screenManager.splashScreenGameComponent);
+        }
+
+        private void AddInitialScreens()
+        {
+            // Activate the first screens.
+            screenManager.AddScreen(new BackgroundScreen(), null);
+
+            // We have different menus for Windows Phone to take advantage of the touch interface
+#if WINDOWS_PHONE
+            screenManager.AddScreen(new PhoneMainMenuScreen(), null);
+#else
+            screenManager.AddScreen(new MainMenuScreen(), null);
+#endif
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+        #endregion
+
+#if WINDOWS_PHONE
+        /// <summary>
+        /// Helper method to the initialize the game to be a portrait game.
+        /// </summary>
+        private void InitializePortraitGraphics()
+        {
+            graphics.PreferredBackBufferWidth = 480;
+            graphics.PreferredBackBufferHeight = 800;
+        }
+
+        /// <summary>
+        /// Helper method to initialize the game to be a landscape game.
+        /// </summary>
+        private void InitializeLandscapeGraphics()
+        {
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 480;
+        }
+
+        void GameLaunching(object sender, Microsoft.Phone.Shell.LaunchingEventArgs e)
+        {
+            AddInitialScreens();
+        }
+
+        void GameActivated(object sender, Microsoft.Phone.Shell.ActivatedEventArgs e)
+        {
+            // Try to deserialize the screen manager
+            if (!screenManager.Activate(e.IsApplicationInstancePreserved))
+            {
+                // If the screen manager fails to deserialize, add the initial screens
+                AddInitialScreens();
+            }
+        }
+
+        void GameDeactivated(object sender, Microsoft.Phone.Shell.DeactivatedEventArgs e)
+        {
+            // Serialize the screen manager when the game deactivated
+            screenManager.Deactivate();
+        }
+#endif
+
+        #region Update Methods
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Enables game input / control when not editing the scene (the editor provides its own control).
+            if (!screenManager.sceneInterface.Editor.EditorAttached)
+            {
+                if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                    this.Exit();
+            }
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+        #endregion
+
+        #region Draw Methods
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            // Check to see if the splash screen is finished.
+            if (!SplashScreenGameComponent.DisplayComplete)
+            {
+                base.Draw(gameTime);
+                return;
+            }
+
+            base.Draw(gameTime);
+        }
+        #endregion
+
+        #region Main entry point
+#if !WINDOWS_PHONE
+        static class Program
+        {
+            /// <summary>
+            /// The main entry point for the application.
+            /// </summary>
+            [STAThread]
+            static void Main(string[] args)
+            {
+#if WINDOWS
+                // Improved ui.
+                System.Windows.Forms.Application.EnableVisualStyles();
+#endif
+
+                using (StarterGame game = new StarterGame())
+                    game.Run();
+            }
+        }
+#endif
+        #endregion
+    }
+}
